8f83b56d6eed4dd35e945fe6979c6a055219c8e3
James Krcelich <james.krcelich@sbcglobal.net>
2026-02-07 17:34:26 -0600

    working updates

--- app.js
+++ app.js
@@ -1,10 +1,40 @@
-console.log("Hello World!\n==========\n");
-console.log(
-  "Follow the steps in the README.md file to complete the exercises:\n==========\n"
+console.log("Hello World!\n============\n");
+console.log("Follow the steps in the README.md file to complete the exercises:\n=================================================================\n"
 );
 
 // Exercise 1
+const firstName = "James";
+let lastName = "Brown";
+let age = 29;
+
+console.log("Exercise 1:", firstName, lastName, "is", age, "years old");
 
 // Exercise 2
+lastName = "Krcelich";
+console.log("Exercise 2:", firstName, lastName, "is", age, "years old");
 
 // Exercise 3
+let language = "Javascript";
+let createdYear = 1995;
+let isCaseSensitive = true;
+
+console.log("Exercise 3: The", language, "language was created in", createdYear);
+
+// Exercise 4
+let price = 19.99;
+let isOnSale = false;
+let salesPercentage = 15;
+let stock = 0;
+let inStock = false;
+let selectedSize = "M";
+
+console.log("Exercise 4: My new hoodie cost $", price, "and the size is", selectedSize);
+
+// Exercise 5
+let title = "Name of the Wind";
+let author = "Patrick Rothfuss";
+let pageCount = 722;
+let bookmark = 456;
+let hasRead = true;
+
+console.log("Exercise 5: The recommended book name is", title, "and it has", pageCount, "pages");
